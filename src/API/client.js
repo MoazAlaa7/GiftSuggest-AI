4804bd58cb2ef7b252faa05cb253ec5976b73ad9
MoazAlaa7 <moazalaa4727@gmail.com>
2026-08-03 04:36:09 +0300

    Setting restrictions for model response

--- src/API/client.js
+++ src/API/client.js
@@ -7,16 +7,29 @@ const client = new OpenAI({
   dangerouslyAllowBrowser: true,
 });
 
+const systemMessage = {
+  role: "system",
+  content: `
+            "If a question is outside of gift suggestions, politely decline to answer."
+            "Skip intros and conclusions. Only output gift suggestions."
+            "Answer ONLY in plain text."
+            "Adhere strictly to the user's specified price range (if provided)."
+            "Do not recommend age-inappropriate, legally restricted, or sensitive items (e.g., alcohol, weapons, tobacco, or adult content)"`,
+};
+
 export default async function getSuggestions() {
   const prompt = "Suggest some gifts for someone who loves cats";
 
   console.log("Prompt:", prompt);
   console.log("Making AI request...");
 
+  //    "If a question is outside of data analysis, politely decline to answer"
+
   try {
     const response = await client.chat.completions.create({
       model: AI_MODEL,
       messages: [
+        systemMessage,
         {
           role: "user",
           content: prompt,
