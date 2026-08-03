3bbbec6f09dbe1b266c67c3c193e7ae7fc9d63e6
MoazAlaa7 <moazalaa4727@gmail.com>
2026-08-04 02:46:34 +0300

    remove file

--- env.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = "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";
-const API_URL = "https://api.groq.com/openai/v1";
-const AI_MODEL = "openai/gpt-oss-120b";
-
-export { API_KEY, API_URL, AI_MODEL };
