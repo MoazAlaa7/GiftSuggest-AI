4f2debe0817c3672673e9d56456583794c06c3a9
MoazAlaa7 <moazalaa4727@gmail.com>
2026-08-04 15:09:40 +0300

    Update conf

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
+
+  // Node (Express)
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["server.js"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+
+  // Browser (React)
+  {
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -18,4 +28,4 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
-])
+]);
